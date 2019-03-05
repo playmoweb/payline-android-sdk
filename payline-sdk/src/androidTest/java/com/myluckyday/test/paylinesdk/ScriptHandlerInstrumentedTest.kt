0029cc2d5c3b7f743b06bc4d32dc53322391b775
Carlos <cchastagnier@myluckyday.fr>
2019-03-05 17:26:01 +0100

    Fixed tests

--- payline-sdk/src/androidTest/java/com/myluckyday/test/paylinesdk/ScriptHandlerInstrumentedTest.kt
+++ payline-sdk/src/androidTest/java/com/myluckyday/test/paylinesdk/ScriptHandlerInstrumentedTest.kt
@@ -19,7 +19,7 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-
+@RunWith(AndroidJUnit4::class)
 class ScriptHandlerInstrumentedTest: PaymentControllerListener{
 
     var paymentController = PaymentController()
